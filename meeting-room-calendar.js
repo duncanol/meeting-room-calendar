b60b6ad6abffbc21a3d5e78c52006cb50309b32a
Duncan Watson <duncan.watson@technophobia.com>
2013-12-31 17:34:45 +0000

    Added template

--- meeting-room-calendar.js
+++ meeting-room-calendar.js
@@ -1,5 +1,10 @@
 if (Meteor.isClient) {
     console.log("The client is ready!");
+    
+    // templates are converted to javascript functions under the Templates namespace
+    Template.intro.showIntro = function() {
+        return Session.equals("showIntro", true);
+    };
 }
 
 if (Meteor.isServer) {
