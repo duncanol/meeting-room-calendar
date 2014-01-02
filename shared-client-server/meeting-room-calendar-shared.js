fd4d093ee30ddb05e45626f5699918d6e388e085
Duncan Watson <duncan.watson@technophobia.com>
2014-01-02 09:10:43 +0000

    Restructured into supported folder structures to separate out concerns

---
+++ shared-client-server/meeting-room-calendar-shared.js
@@ -0,0 +1,17 @@
+assets = new Meteor.Collection('assets');
+
+assets.allow({
+    insert: function (userId, doc) {
+        return uniqueName(doc.name);
+    },
+    update: function (userId, doc, fields, modifier) {
+        return uniqueName(modifier['$set'].name);
+    },
+    remove: function (userId, doc, fields, modifier) {
+        return true;
+    }
+});
+
+var uniqueName = function(assetName) {
+    return assets.find({name: assetName}).count() == 0;
+};
